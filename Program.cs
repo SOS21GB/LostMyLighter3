70f7b23904b4a19699ba9bb0542875604b806e8f
Daniel Frones <daniel.frones@gmail.com>
2021-11-11 12:01:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LighterGrp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
