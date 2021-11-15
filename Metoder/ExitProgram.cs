f2bc217abe3418ca0c265e6e602a7a9f332bcccc
Ida <89834477+IdasJohansson@users.noreply.github.com>
2021-11-15 11:45:29 +0100

    Main menu (#8)

---
+++ Metoder/ExitProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LighterGrp3.Metoder
+{
+    public class ExitProgram
+    {
+        // Metod som skriver ut THE END! om användaren väljer option 7 i MainMenu
+        public static void EndMessage(string text = "THE END!", int delay = 200)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+                System.Threading.Thread.Sleep(delay);
+
+                // För att hoppa över animationen och få texten utskriven direkt
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        Console.Write(text.Substring(i + 1));
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
