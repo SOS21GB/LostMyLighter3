e3a8dd35cecab98487b3d014bd2b300b4a9ea56b
Victoria Hering Röhr <victoriaerohr@gmail.com>
2021-11-15 14:40:13 +0100

    uppdatera

--- Klass/Marshall.cs
+++ Klass/Marshall.cs
@@ -1,20 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LighterGrp3.Klass
 {
     class Marshall
     {
+        private static List<Marshall> marshalls = new List<Marshall>();
+
         private int _nr = 0;                           //marshallNr - ska den räkna upp för varje så ID blir 11,12,53,24 osv eller 11,12,51,21?
         private int _id;                               //userId + marshallNr
-        private bool _active = false;                  //
         private string _brand = "Unknown";             //märke på marshall
-        private string _address;                       //adress 
-        private int _postalCode;                       //postnummer
+        private bool _active = false;                  //
         private DateTime _lifeExp;                     //hur länge förväntas den brinna? Använd DateTime
         private DateTime _expOut;                      //när förväntas den slockna? Använd DateTime
         private DateTime _regTime;                     //när den registrerades. Använd DateTime
+        private string _address;                       //adress 
+        private int _postalCode;                       //postnummer
         private string _regByUser;                     //userId + eventuellt userName
 
+        public Marshall(int nr, int id, bool active, string brand, string address, int postalCode, DateTime lifeExp, DateTime expOut, DateTime regTime, string regByUser)
+        //konstruktor med all info
+        {
+            this._nr = nr;
+            this._id = id;
+            this._brand = brand;
+            this._active = active;
+            this._lifeExp = lifeExp;
+            this._expOut = expOut;
+            this._regTime = regTime;
+            this._address = address;
+            this._postalCode = postalCode;
+            this._regByUser = regByUser;
+            marshalls.Add(this);
+        }
+        public Marshall(string brand, DateTime lifeExp)
+        //konstruktor med märkesinfo
+        {
+            this._brand = brand;
+            this._lifeExp = lifeExp;
+        }
 
         public int NR
         {
@@ -128,34 +152,5 @@ namespace LighterGrp3.Klass
         }
 
         //Marshall marshall = new Marshall();
-
-
-        public Marshall()
-        //tom konstruktor
-        {
-        }
-        public Marshall(int id, bool active, string brand, string address, int postalCode, DateTime lifeExp, DateTime expOut, DateTime regTime, string regByUser)
-        //konstruktor med all info
-        {
-            this._id = id;
-            this._active = active;
-            this._brand = brand;
-            this._address = address;
-            this._postalCode = postalCode;
-            this._lifeExp = lifeExp;
-            this._expOut = expOut;
-            this._regTime = regTime;
-            this._regByUser = regByUser;
-        }
-        public Marshall(string brand, DateTime lifeExp)
-        //konstruktor med märkesinfo
-        {
-            this._brand = brand;
-            this._lifeExp = lifeExp;
-        }
-
-        public static void New()
-        {
-        }
     }
 }
