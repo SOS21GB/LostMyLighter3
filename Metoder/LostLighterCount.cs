cae2c1ae735e41f9265f80309e33b2d1d79d9110
Ida <89834477+IdasJohansson@users.noreply.github.com>
2021-11-16 11:19:37 +0100

    Skapat metoden LostLighterCounter samt lagt till data för  befintliga användare och marschaller (#13)

---
+++ Metoder/LostLighterCount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LighterGrp3.Klass; 
+
+namespace LighterGrp3.Metoder
+{
+    public class LostLighterCount
+    {
+
+        /*
+        public void AddLostLighter()
+        {
+            NumberOfLostLighters++;
+            Console.WriteLine("Number of lost lighters: {0}", NumberOfLostLighters);
+            HeadMenu.MainMenu();
+        }
+        */
+
+        public LostLighterCount()
+        {
+        }
+    }
+}
