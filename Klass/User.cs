3340d467077c5eda41f51463be12e2b100ac0487
Mickelainen <90410529+Mickelainen@users.noreply.github.com>
2021-11-16 11:58:23 +0100

    La till printa specifik user (#17)

--- Klass/User.cs
+++ Klass/User.cs
@@ -35,7 +35,33 @@ namespace LighterGrp3.Klass
             this.NumberOfLostLighters = numberoflostlighters;
             users.Add(this);
         }
-        
+
+        public void PrintCurrentUser()
+        {
+            Console.WriteLine("ID : {0}", ID);
+            Console.WriteLine("Name : {0}", Name);
+            Console.WriteLine("Age : {0}", Age);
+            Console.WriteLine("Adress : {0}", Adress);
+            Console.WriteLine("Number of searches : {0}", NumberOfSearches);
+            Console.WriteLine("Number of registered lighters : {0}", NumberOfRegisteredLighters);
+            Console.WriteLine("Number of lost lighters : {0}", NumberOfLostLighters);
+
+        }
+
+
+        public static User GetUserId(int id)
+        {
+            foreach (User u in User.users)
+            {
+                if (u.ID == id)
+                {
+                    return u;
+                }
+            }
+            return null;
+        }
+
+
         public void AddSearchCount()
         {
             NumberOfSearches++;
