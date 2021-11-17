a8fe98f9ea38e50fdccd399eab2b362ad0321d65
MariaSliufko <maria.sliufko@icloud.com>
2021-11-17 12:05:50 +0100

    fixec LogIn

--- Metoder/LogIn.cs
+++ Metoder/LogIn.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using LighterGrp3.Klass;
 namespace LighterGrp3.Metoder
+
+
 {
     public class LogIn
     {
-        public LogIn()
+        public static User LogInMethod()
         {
+            Console.WriteLine("Enter you're userid: ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            foreach (KeyValuePair<int, User> user in User.users)
+            {
+                Console.WriteLine("");
+
+                if (user.Value.ID == answer)
+                {
+                    Console.WriteLine("Successfull Login");
+                    return user.Value;
+                }
+            }
+            return null;
         }
     }
 }
