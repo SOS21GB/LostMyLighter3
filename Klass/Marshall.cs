3603ff5184b32af11458df4412d3822e52bcf091
Vipsisen <89784736+Vipsisen@users.noreply.github.com>
2021-11-17 13:58:55 +0100

    - (#25)

--- Klass/Marshall.cs
+++ Klass/Marshall.cs
@@ -8,21 +8,18 @@ namespace LighterGrp3.Klass
     {
         public static List<Marshall> marshalls = new List<Marshall>();
 
-        private int _nr = 0;                           //marshallNr - ska den räkna upp för varje så ID blir 11,12,53,24 osv eller 11,12,51,21?
+        protected int _nr = 0;                           //marshallNr - ska den räkna upp för varje så ID blir 11,12,53,24 osv eller 11,12,51,21?
         private int _id;                               //userId + marshallNr
         private string _brand = "Unknown";             //märke på marshall
         private bool _active = false;                  //
-        private DateTime _lifeExp;                     //hur länge förväntas den brinna? Använd DateTime
-        private DateTime _expOut;                      //när förväntas den slockna? Använd DateTime
         private DateTime _regTime;                     //när den registrerades. Använd DateTime
+        private int _lifeExp;                     //hur länge förväntas den brinna? Använd DateTime
+        private DateTime _expOut;                      //när förväntas den slockna? Använd DateTime
         private string _address;                       //adress 
         private int _postalCode;                       //postnummer
-        private string _regByUser;                     //userId + eventuellt userName
-        public Marshall()
-        {
+        private int _regByUser = 0;                     //userId + eventuellt userName
 
-        }
-        public Marshall(int nr, int id, bool active, string brand, string address, int postalCode, DateTime lifeExp, DateTime expOut, DateTime regTime, string regByUser)
+        public Marshall(int nr, int id, bool active, string brand, string address, int postalCode, int lifeExp, DateTime expOut, DateTime regTime, int regByUser)
         //konstruktor med all info
         {
             this._nr = nr;
@@ -37,11 +34,15 @@ namespace LighterGrp3.Klass
             this._regByUser = regByUser;
             marshalls.Add(this);
         }
-        public Marshall(string brand, DateTime lifeExp)
+        public Marshall(string brand, int lifeExp)
         //konstruktor med märkesinfo
         {
             this._brand = brand;
             this._lifeExp = lifeExp;
+        }
+
+        public Marshall()
+        {
         }
 
         public int NR
@@ -110,7 +111,7 @@ namespace LighterGrp3.Klass
                 _postalCode = value;
             }
         }
-        public DateTime LifeExp
+        public int LifeExp
         {
             get
             {
@@ -143,7 +144,7 @@ namespace LighterGrp3.Klass
                 _regTime = value;
             }
         }
-        public string RegByUser
+        public int RegByUser
         {
             get
             {
