c4a3d9019ab8e3cb02c4394b76d991bf5c513379
Victoria Hering Röhr <victoriaerohr@gmail.com>
2021-11-17 08:38:05 +0100

    -

--- Program.cs
+++ Program.cs
@@ -9,11 +9,7 @@ namespace LighterGrp3
     {
         static void Main(string[] args)
         {
-            
-            
-
-            
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
