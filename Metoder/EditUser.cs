3a208da7fc98f976ac691fecf4091b9f162b4f1b
MariaSliufko <89866298+MariaSliufko@users.noreply.github.com>
2021-11-18 13:11:19 +0100

    Create EditUser.cs (#33)
    Nice!

---
+++ Metoder/EditUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using LighterGrp3.Klass;
+namespace LighterGrp3.Metoder
+
+{
+    public static class EditUser
+    {
+       
+
+            // Kunna göra ändringar i användaren. o T.ex. Byta namn, ändra ålder.
+            //Peka sedan mot current user
+
+
+            public static void EditUserMethod()
+            {
+
+                Console.WriteLine("Edit name");
+                CurrentUser.current.Name = Console.ReadLine();
+
+                Console.WriteLine("Edit age");
+                CurrentUser.current.Age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Edit address");
+                CurrentUser.current.Adress = Console.ReadLine();
+
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+
